4291cf74465aaedc80047b92a3ed204bc7de09f2
tkingless <t.kingless@yahoo.com>
2017-10-02 10:51:30 +0800

    update loading qml

--- PyQts/launcher.py
+++ PyQts/launcher.py
@@ -14,7 +14,6 @@ class ThymioImplQObj(QtCore.QObject):
 
         self.thymioCltr = ThymioController("/home/tkingless/Development/Github/ThymioOfficeAssistant/PyQts/tmpl.aesl")
 
-    #TODO callback/slot
     #@QtCore.pyqtSlot(float,float)
     def on_joystick_moved(self,x,y):
     	#print 'joystick_moved() x: {0}, y:{1}'.format(x,y)
@@ -41,18 +40,13 @@ if __name__ == '__main__':
     mainModel = ThymioImplQObj()
 
     engine.rootContext().setContextObject(mainModel)
-    canvas.setSource(QtCore.QUrl.fromLocalFile('virtual_joystick.qml')) 
+    #canvas.setSource(QtCore.QUrl.fromLocalFile('virtual_joystick.qml')) 
+    canvas.setSource(QtCore.QUrl('virtual_joystick.qml')) 
 
     #http://pyqt.sourceforge.net/Docs/PyQt4/qml.html
     #also reverse way can be done, this is cleaner for separation
     rootObject = canvas.rootObject()
     rootObject.joystick_moved.connect(mainModel.on_joystick_moved)
-
-    """TODO 
-    using qrc not working, e.g. 
-    "view.setSource(QtCore.QUrl('qrc:/res/virtual_joystick.qml'))"
-    How .qrc be used? check document
-    """
 
     canvas.setGeometry(QtCore.QRect(100, 100, 450, 450)) #TODO get the property from qml, the root.width & root.height
     canvas.show()
